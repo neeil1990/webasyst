7a392d47af9b0f68c492318d99435929736071cd
neeil1990 <neeil@mail.ru>
2018-02-04 14:13:14 +0300

    nbimagepost

--- wa-apps/blog/plugins/nbimagepost/lib/actions/blogAjax.actions.php
+++ wa-apps/blog/plugins/nbimagepost/lib/actions/blogAjax.actions.php
@@ -14,6 +14,7 @@ class blogAjaxActions extends waJsonActions
                 $file_name = md5($_FILES['file']['name']).str_replace('/','.',strstr($_FILES['file']['type'],'/'));
                 $files = '/wa-data/public/blog/img/' . $file_name;
                 $files_root = $_SERVER['DOCUMENT_ROOT'].$files;
+                waFiles::create(wa()->getConfig()->getPath('data').'/public/blog/img');
                 move_uploaded_file($_FILES['file']['tmp_name'], $files_root);
             }else{
                 $files = false;
@@ -31,14 +32,19 @@ class blogAjaxActions extends waJsonActions
         $id = waRequest::post('id');
         $href = waRequest::post('href');
 
-        if($x and $y and $w and $h and $href){
+        if($w and $h and $href){
+             $new_home_path = '/public/blog/img/'.ceil($w).'x'.ceil($h);
+             $new_root_path = wa()->getConfig()->getPath('data');
+             $new_file_name = md5($href).strstr($href,'.');
+
+             waFiles::create($new_root_path.$new_home_path);
 
              $image = waImage::factory(wa()->getConfig()->getRootPath().$href);
              $image->crop($w, $h, $x, $y);
-             $image->save(null, 100);
+             $image->save($new_root_path.$new_home_path.'/'.$new_file_name, 100);
              $model = new blogNbimagepostModel();
-             $model->updateById($id, array("image" => $href));
-             $this->response = $href;
+             $model->updateById($id, array("image" => '/wa-data'.$new_home_path.'/'.$new_file_name));
+             $this->response = '/wa-data'.$new_home_path.'/'.$new_file_name;
         }
     }
 }
